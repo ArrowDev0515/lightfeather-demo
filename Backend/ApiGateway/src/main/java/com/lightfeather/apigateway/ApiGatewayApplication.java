30ffd356b194ff96b538f465bc7ba9c8a5a13361
Li.Jett <l.jett124@gmail.com>
2022-11-05 19:22:30 +0900

    finish cloud microservice configuration.

--- Backend/ApiGateway/src/main/java/com/lightfeather/apigateway/ApiGatewayApplication.java
+++ Backend/ApiGateway/src/main/java/com/lightfeather/apigateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package com.lightfeather.BackendService;
+package com.lightfeather.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
-public class BackendServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
