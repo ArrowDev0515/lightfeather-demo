b783503d56314b0f7e449178ab5218023c454e72
Li.Jett <l.jett124@gmail.com>
2022-11-05 19:26:17 +0900

    finished prototype

---
+++ Backend/ApiGateway/src/test/java/com/lightfeather/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightfeather.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Backend/BackendService/src/main/java/com/lightfeather/BackendService/BackendServiceApplication.java
+++ Backend/BackendService/src/main/java/com/lightfeather/BackendService/BackendServiceApplication.java
@@ -3,11 +3,9 @@ package com.lightfeather.BackendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
 public class BackendServiceApplication {
 
 	public static void main(String[] args) {
