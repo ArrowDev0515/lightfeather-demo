19819f5e9a0bc9ad61ee83c229260b151c8c53da
Li.Jett <l.jett124@gmail.com>
2022-11-08 16:06:43 +0900

    remove discovery client.

--- Backend/ApiGateway/src/main/java/com/lightfeather/apigateway/ApiGatewayApplication.java
+++ Backend/ApiGateway/src/main/java/com/lightfeather/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,10 @@ package com.lightfeather.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
